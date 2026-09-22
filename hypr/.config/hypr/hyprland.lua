01a73ae91c3d28e62b35406c89671be88280ade1
vladimirrainov <raynov.vladimir@gmail.com>
2026-09-22 16:53:08 +0100

    fuzzel, mako and hyprsunset configured

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -348,6 +348,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("mako")
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("hypridle")
+    hl.exec_cmd("hyprsunset")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")   -- Stores only text data
     hl.exec_cmd("wl-paste --type image --watch cliphist store")  -- Stores only image data
 end)
